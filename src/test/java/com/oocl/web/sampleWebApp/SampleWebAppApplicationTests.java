02b84a1570d37570599ceb28164bfcba1dd3cc99
J <jason19951020@gmail.com>
2018-11-29 00:07:10 +0800

    add test

--- src/test/java/com/oocl/web/sampleWebApp/SampleWebAppApplicationTests.java
+++ src/test/java/com/oocl/web/sampleWebApp/SampleWebAppApplicationTests.java
@@ -2,15 +2,31 @@ package com.oocl.web.sampleWebApp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class SampleWebAppApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockmvc;
 
+    @Test
+    public void testReturnStatusAndLocation() throws Exception {
+        String json = "{ \"username\": \"string\" }";
+        this.mockmvc.perform(post("/users").content(json)).andDo(print()).andExpect(status().isCreated())
+                .andExpect(header().string("Location", "/users/string"));
+
+    }
 }
